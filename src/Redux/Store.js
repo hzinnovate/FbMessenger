75ca7dc1268e40c2ef2651aae9d60b5d5dd869d3
Syed Hasnain Zaidi <39019111+syedzaidi95@users.noreply.github.com>
2019-12-27 19:58:58 +0500

    Update Store.js

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { AsyncStorage } from 'react-native';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer'
 
-const persistConfig = { key: 'root', storage, }
+const persistConfig = { key: 'root', storage: AsyncStorage }
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = createStore(persistedReducer, applyMiddleware(thunk));
 const persistor = persistStore(store);
